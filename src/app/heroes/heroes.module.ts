ce4081040034c56cf05241093f6876c1ec3c852f
luisangelnav12 <luisangelnav12@gmail.com>
2021-09-06 17:48:43 -0500

    create module heroes

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+
+@NgModule({
+  declarations: [HeroeComponent, ListadoComponent],
+  imports: [],
+})
+export class HeroesModule {}
